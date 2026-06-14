7e33bff8878247deb20b740a5f24f8b02d8f8914
megersadawit3-png <megersadawit3@gmail.com>
2026-06-14 08:39:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-import os
+# config.py
 
-BOT_TOKEN = os.getenv("8130120024:AAEc6Yp0beD6xOIxikfU1EMJSPp83pp52lo")
+# የቦት ቶከኑ ግዴታ በጥቅስ ምልክት (" ") መታጠር አለበት!
+BOT_TOKEN = "8130120024:AAEc6Yp0beD6xOIxikfU1EMJSPp83pp52lo"
+
+ADMIN_ID = @Batuel012  # ያንተን የቴሌግራም ID ቁጥር ብቻ እዚህ አስገባ
+TELEBIRR_NUMBER = "0947843445"
+TELEBIRR_NAME = "Dawit Megersa"
+DB_NAME = "kitab.db"
