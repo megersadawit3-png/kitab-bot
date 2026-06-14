f474a5a8d0e2d17933267be6cc5f24379ba00a81
megersadawit3-png <megersadawit3@gmail.com>
2026-06-14 09:19:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # የቦት ቶከኑ ግዴታ በጥቅስ ምልክት (" ") መታጠር አለበት!
 BOT_TOKEN = "8130120024:AAEc6Yp0beD6xOIxikfU1EMJSPp83pp52lo"
 
-ADMIN_ID = @Batuel012  # ያንተን የቴሌግራም ID ቁጥር ብቻ እዚህ አስገባ
+ADMIN_ID = 8009705665
 TELEBIRR_NUMBER = "0947843445"
 TELEBIRR_NAME = "Dawit Megersa"
 DB_NAME = "kitab.db"
